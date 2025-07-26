8ccc8de1596c02112b798f827160095888dc4efb
Graham Arnett <GrahamKentArnett@outlook.com>
2025-07-25 20:54:49 -0400

    I broke it again it seems

--- SillyDiceGame/Program.cs
+++ SillyDiceGame/Program.cs
@@ -13,17 +13,21 @@
                 int score = 0;
                 for(int i = 0;i < 2; i++)
                 {
-                    int roll = random.Next(1,7);
-                    score += roll;
-                    if(score > 21)
+                    do
                     {
-                        Console.WriteLine("You have lost.");
-                    }
-                    else
-                    {
-                        Console.Write("\n\nWould you like to draw again? (y/n): ");
-
-                    }
+                        int roll = random.Next(1, 7);
+                        score += roll;
+                        if (score > 21)
+                        {
+                            Console.WriteLine("You have lost.");
+                        }
+                        else
+                        {
+                            Console.Write("\n\nWould you like to draw again? (y/n): ");
+                            //redo = Console.ReadKey().KeyChar;
+                        }
+                        redo = Console.ReadKey().KeyChar;
+                    } while (redo == 'y'|| redo == 'Y');
                 }
                 Console.Write("\n\nWould you like to go again? (y/n): ");
                 redo = Console.ReadKey().KeyChar;
